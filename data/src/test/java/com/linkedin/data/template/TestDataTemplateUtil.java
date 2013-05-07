ca4fc35de5646515524e75900d92762c7f395b2b
Swee Lim <slim@linkedin.com>
2013-05-06 20:13:21 -0700

    Add copy() and clone() to non-record generated DataTemplate classes

---
+++ data/src/test/java/com/linkedin/data/template/TestDataTemplateUtil.java
@@ -0,0 +1,98 @@
+/*
+ Copyright (c) 2012 LinkedIn Corp.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package com.linkedin.data.template;
+
+
+import com.linkedin.data.TestUtil;
+import com.linkedin.data.schema.RecordDataSchema;
+import java.lang.reflect.Method;
+
+public class TestDataTemplateUtil
+{
+  public static class FieldInfo
+  {
+    private final RecordDataSchema.Field _field;
+    private final Class<?> _fieldClass;
+
+    public FieldInfo(RecordDataSchema.Field field, Class<?> fieldClass)
+    {
+      _field = field;
+      _fieldClass = fieldClass;
+    }
+
+    public RecordDataSchema.Field getField()
+    {
+      return _field;
+    }
+
+    public Class<?> getFieldClass()
+    {
+      return _fieldClass;
+    }
+  }
+
+  public static FieldInfo fieldInfo(RecordTemplate recordTemplate, String fieldName)
+  {
+    RecordDataSchema schema = recordTemplate.schema();
+    RecordDataSchema.Field field = schema.getField(fieldName);
+    String getterName = methodName("get", fieldName);
+    try
+    {
+      Method method = recordTemplate.getClass().getMethod(getterName);
+      Class<?> fieldClass = method.getReturnType();
+      return new FieldInfo(field, fieldClass);
+    }
+    catch (NoSuchMethodException e)
+    {
+      throw new IllegalStateException("Cannot find method " + getterName, e);
+    }
+  }
+
+  public static String capitalizeFirstCharacter(String s)
+  {
+    if (s != null && s.isEmpty() == false && Character.isLowerCase(s.charAt(0)))
+    {
+      return Character.toUpperCase(s.charAt(0)) + s.substring(1);
+    }
+    else
+    {
+      return s;
+    }
+  }
+
+  public static String methodName(String prefix, String fieldName)
+  {
+    return prefix + capitalizeFirstCharacter(fieldName);
+  }
+
+
+  public static void assertCloneAndCopyReturnType(Class<? extends DataTemplate> dataTemplateClass)
+  {
+    String methodNames[] = { "clone", "copy" };
+    for (String methodName : methodNames)
+    {
+      try
+      {
+        assert(dataTemplateClass.getMethod(methodName).getReturnType() == dataTemplateClass);
+      }
+      catch (NoSuchMethodException e)
+      {
+        throw new IllegalArgumentException("Cannot get " + methodName + " method from " + dataTemplateClass.getSimpleName(), e);
+      }
+    }
+  }
+}
---
+++ generator-test/src/test/java/com/linkedin/pegasus/generator/test/TestFixed.java
@@ -0,0 +1,51 @@
+/*
+   Copyright (c) 2013 LinkedIn Corp.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package com.linkedin.pegasus.generator.test;
+
+
+import com.linkedin.data.template.TestDataTemplateUtil;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertSame;
+
+
+public class TestFixed
+{
+  @Test
+  public void testClone() throws CloneNotSupportedException
+  {
+    FixedMD5 fixed = new FixedMD5("0123456789abcdef");
+    FixedMD5 fixedClone = fixed.clone();
+
+    assertSame(fixedClone.data(), fixed.data());
+  }
+
+  @Test
+  public void testCopy() throws CloneNotSupportedException
+  {
+    FixedMD5 fixed = new FixedMD5("0123456789abcdef");
+    FixedMD5 fixedCopy = fixed.copy();
+
+    assertSame(fixedCopy.data(), fixed.data());
+  }
+
+  @Test
+  public void testCloneAndCopyReturnType() throws NoSuchMethodException
+  {
+    TestDataTemplateUtil.assertCloneAndCopyReturnType(Fixed16.class);
+  }
+}
