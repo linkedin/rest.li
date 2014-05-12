c7f6d9790f01827d432ab6f30b9619108d3b5bfa
Joe Betz <jbetz@linkedin.com>
2014-05-12 15:02:01 -0700

    Clean up caprep so it can be better leveraged for language independent test suite

--- r2/src/main/java/com/linkedin/r2/caprep/db/DefaultMessageSerializer.java
+++ r2/src/main/java/com/linkedin/r2/caprep/db/DefaultMessageSerializer.java
@@ -222,7 +222,7 @@ public class DefaultMessageSerializer implements MessageSerializer
       {
         valueBuilder.append(line);
       }
-      builder.addHeaderValue(key, valueBuilder.toString());
+      builder.addHeaderValue(key, valueBuilder.toString().trim());
     }
   }
 
@@ -275,19 +275,73 @@ public class DefaultMessageSerializer implements MessageSerializer
     write(out, HTTP_1_1);
     write(out, SP);
 
+    int statusCode;
     if (res instanceof RestResponse)
     {
-      write(out, Integer.toString(((RestResponse)res).getStatus()));
+      statusCode = ((RestResponse) res).getStatus();
+      write(out, Integer.toString(statusCode));
     }
     else
     {
+      statusCode = 200;
       write(out, STATUS_200);
     }
     write(out, SP);
 
-    // We can write whatever we like here per RFC-2616
-    write(out, "No Reason Phrase");
+
+    write(out, toReasonPhrase(statusCode));
     write(out, CRLF);
+  }
+
+  /**
+   * Converts all status codes defined by the official http spec to their official reason phrase.  Unrecognized status
+   * codes will be converted to "No Reason Phrase", which is safe to do per RFC-2616.
+   */
+  private String toReasonPhrase(int status)
+  {
+    switch(status)
+    {
+      case 200: return "OK";
+      case 201: return "Created";
+      case 202: return "Accepted";
+      case 203: return "Non-Authoritative Information";
+      case 204: return "No Content";
+      case 205: return "Reset Content";
+      case 206: return "Partial Content";
+      case 300: return "Multiple Choices";
+      case 301: return "Moved Permanently";
+      case 302: return "Found";
+      case 303: return "See Other";
+      case 304: return "Not Modified";
+      case 305: return "Use Proxy";
+      case 306: return "Switch Proxy";
+      case 307: return "Temporary Redirect";
+      case 400: return "Bad Request";
+      case 401: return "Unauthorized";
+      case 402: return "Payment Required";
+      case 403: return "Forbidden";
+      case 404: return "Not Found";
+      case 405: return "Method Not Allowed";
+      case 406: return "Not Acceptable";
+      case 407: return "Proxy Authentication Required";
+      case 408: return "Request Timeout";
+      case 409: return "Conflict";
+      case 410: return "Gone";
+      case 411: return "Length Required";
+      case 412: return "Precondition Failed";
+      case 413: return "Request Entity Too Large";
+      case 414: return "Request-URI Too Long";
+      case 415: return "Unsupported Media Type";
+      case 416: return "Requested Range Not Satisfiable";
+      case 417: return "Expectation Failed";
+      case 500: return "Internal Server Error";
+      case 501: return "Not Implemented";
+      case 502: return "Bad Gateway";
+      case 503: return "Service Unavailable";
+      case 504: return "Gateway Timeout";
+      case 505: return "HTTP Version Not Supported";
+      default: return "No Reason Phrase"; // For any cases not covered, this is okay, since we can write whatever we like here per RFC-2616
+    }
   }
 
   private void writeHeaders(OutputStream out, RestMessage msg) throws IOException
