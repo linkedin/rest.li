4b802ab20a7f421e72105893349bf71cf8394d5f
Ang Xu <axu@linkedin.com>
2016-02-04 15:43:52 -0800

    Fix a bug in ZKFSUtil where the trailing '/' is not removed correctly
    RB=655371
    G=si-core-reviewers
    R=ssheng,xzhu,dhoa,cxu,zzhu
    A=zzhu,cxu

--- d2/src/main/java/com/linkedin/d2/balancer/zkfs/ZKFSUtil.java
+++ d2/src/main/java/com/linkedin/d2/balancer/zkfs/ZKFSUtil.java
@@ -40,7 +40,7 @@ public class ZKFSUtil
     String normalized = basePath;
     while (normalized.endsWith("/"))
     {
-      normalized = normalized.substring(0, normalized.length() - 2);
+      normalized = normalized.substring(0, normalized.length() - 1);
     }
     return normalized;
   }
