da8ce278846592e04c6f842f3f9dc920a9db3df6
Joshua Hartman <jhartman@linkedin.com>
2017-03-29 18:22:11 -0700

    Moving the logic around creating the BatchKVResponse schema to the schema() method and pulling some variables into constants for efficiency gains since schema is inaccurate and rarely used in this class.
    RB=956032
    G=si-dev
    R=kjin,mnchen
    A=mnchen

--- restli-client/src/main/java/com/linkedin/restli/client/response/BatchKVResponse.java
+++ restli-client/src/main/java/com/linkedin/restli/client/response/BatchKVResponse.java
@@ -49,6 +49,8 @@ public class BatchKVResponse<K, V extends RecordTemplate> extends RecordTemplate
   public static final String RESULTS = "results";
   public static final String ERRORS = "errors";
 
+  private static final String BATCH_KV_RESPONSE_CLASSNAME = BatchKVResponse.class.getSimpleName();
+
   private RecordDataSchema _schema;
   private Class<V> _valueClass;
   private Map<K, V> _results;
@@ -189,7 +191,7 @@ public class BatchKVResponse<K, V extends RecordTemplate> extends RecordTemplate
   {
     super(data, null);
 
-    createSchema(valueType.getType());
+    _valueClass = valueType.getType();
     deserializeData(keyType, keyParts, complexKeyType, version);
   }
 
@@ -213,20 +215,6 @@ public class BatchKVResponse<K, V extends RecordTemplate> extends RecordTemplate
   {
     _valueClass = valueClass;
 
-    final StringBuilder errorMessageBuilder = new StringBuilder(10);
-    final Name elementSchemaName = new Name(valueClass.getSimpleName(), errorMessageBuilder);
-    final MapDataSchema resultsSchema = new MapDataSchema(new RecordDataSchema(elementSchemaName, RecordDataSchema.RecordType.RECORD));
-    final RecordDataSchema.Field resultsField = new RecordDataSchema.Field(resultsSchema);
-    resultsField.setName(RESULTS, errorMessageBuilder);
-
-    final Name errorSchemaName = new Name(ErrorResponse.class.getSimpleName(), errorMessageBuilder);
-    final MapDataSchema errorsSchema = new MapDataSchema(new RecordDataSchema(errorSchemaName, RecordDataSchema.RecordType.RECORD));
-    final RecordDataSchema.Field errorsField = new RecordDataSchema.Field(errorsSchema);
-    errorsField.setName(ERRORS, errorMessageBuilder);
-
-    final Name name = new Name(BatchKVResponse.class.getSimpleName(), errorMessageBuilder);
-    _schema = new RecordDataSchema(name, RecordDataSchema.RecordType.RECORD);
-    _schema.setFields(Arrays.asList(resultsField, errorsField), errorMessageBuilder);
   }
 
   protected void deserializeData(TypeSpec<K> keyType,
@@ -304,6 +292,27 @@ public class BatchKVResponse<K, V extends RecordTemplate> extends RecordTemplate
   @Override
   public RecordDataSchema schema()
   {
+    synchronized (this)
+    {
+      // Don't use double-checked locking because _schema isn't volatile
+      if (_schema == null)
+      {
+        final StringBuilder errorMessageBuilder = new StringBuilder(10);
+        final Name elementSchemaName = new Name(_valueClass.getSimpleName(), errorMessageBuilder);
+        final MapDataSchema resultsSchema = new MapDataSchema(new RecordDataSchema(elementSchemaName, RecordDataSchema.RecordType.RECORD));
+        final RecordDataSchema.Field resultsField = new RecordDataSchema.Field(resultsSchema);
+        resultsField.setName(RESULTS, errorMessageBuilder);
+
+        final MapDataSchema errorsSchema = new MapDataSchema(DataTemplateUtil.getSchema(ErrorResponse.class));
+        final RecordDataSchema.Field errorsField = new RecordDataSchema.Field(errorsSchema);
+        errorsField.setName(ERRORS, errorMessageBuilder);
+
+        final Name name = new Name(BATCH_KV_RESPONSE_CLASSNAME, errorMessageBuilder);
+        _schema = new RecordDataSchema(name, RecordDataSchema.RecordType.RECORD);
+        _schema.setFields(Arrays.asList(resultsField, errorsField), errorMessageBuilder);
+      }
+    }
+
     return _schema;
   }
 
