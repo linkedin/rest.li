6c3614cb37a6608c3f523d265ff8a41f344a122b
Chao Xu <cxu@linkedin.com>
2018-05-03 16:09:18 -0700

    Define ZKAclProvider interface
    ZKAclProvider provides all information a zkclient needs to use zookeeper ACL. It abstracts the ACL management from
    regular zk operations.
    
    RB=1297701
    G=si-core-reviewers
    R=ajing,dhoa,fcapponi
    A=fcapponi

---
+++ d2/src/main/java/com/linkedin/d2/discovery/stores/zk/acl/ZKAclProvider.java
@@ -0,0 +1,50 @@
+/*
+   Copyright (c) 2018 LinkedIn Corp.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package com.linkedin.d2.discovery.stores.zk.acl;
+
+import java.util.List;
+import org.apache.zookeeper.data.ACL;
+
+/**
+ * Provide Zookeeper {@link ACL} information for authentication and authorization
+ *
+ * Two types of information will be provided:
+ *
+ * 1. AuthScheme and AuthInfo: only 'digest' scheme needs this. Zkclient needs to use this info to authenticate
+ *    itself to zookeeper server
+ *
+ * 2. ACL list: a list of ACLs for the zonode. Zookeeper supports combination of different schemes. For example,
+ *    ["world:anyone:read", "digest:admin:adminpass:admin", "hostname:linkedin.com:write"]
+ */
+public interface ZKAclProvider {
+  /**
+   * provide {@link ACL} list for the znode
+   *
+   * @return list of ACLs
+   */
+  List<ACL> getACL();
+
+  /**
+   * provide zookeeper authentication scheme. ZK client uses this scheme for authentication
+   */
+  String getAuthScheme();
+
+  /**
+   * provide zookeeper authentication Data in byte array. Use together with scheme to authenticate the client
+   */
+  byte[] getAuthInfo();
+}
