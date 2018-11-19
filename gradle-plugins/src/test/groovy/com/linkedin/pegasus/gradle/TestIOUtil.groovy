e0cd1bd757a2c9e7d248f41ec47b64830a6584ff
Theodore Ni <tni@linkedin.com>
2018-11-19 15:21:34 -0800

    Use temporary directory in TestIOUtil test.
    The previously hardcoded value runs into conflicts with other projects
    that use the same hardcoded value.
    
    RB=1489722
    G=si-core-reviewers
    R=ssheng,cxu,fcapponi,mnchen
    A=mnchen

--- gradle-plugins/src/test/groovy/com/linkedin/pegasus/gradle/TestIOUtil.groovy
+++ gradle-plugins/src/test/groovy/com/linkedin/pegasus/gradle/TestIOUtil.groovy
@@ -1,18 +1,24 @@
 package com.linkedin.pegasus.gradle
 
-
 import org.gradle.util.GFileUtils
 import org.testng.annotations.Test
+
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
 
 class TestIOUtil {
 
   @Test
-  void writesTextOnNonExistantFile() {
-    def f = new File("/tmp/foo/bar/baz.txt")
+  void writesTextOnNonExistentFile() {
+    Path tempDirectory = Files.createTempDirectory(getClass().getSimpleName())
+    File f = tempDirectory.resolve("foo/bar/baz.txt").toFile()
+
     f.delete()
     f.parentFile.delete()
-    f.deleteOnExit()
-    f.parentFile.deleteOnExit()
+
     assert !f.exists()
     assert !f.parentFile.exists()
 
@@ -21,5 +27,23 @@ class TestIOUtil {
 
     //then
     assert GFileUtils.readFile(f) == "foo"
+
+    deleteDirectory(tempDirectory)
+  }
+
+  private static void deleteDirectory(Path directory) {
+    Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+      @Override
+      FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Files.delete(file)
+        return FileVisitResult.CONTINUE
+      }
+
+      @Override
+      FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        Files.delete(dir)
+        return FileVisitResult.CONTINUE;
+      }
+    });
   }
 }
