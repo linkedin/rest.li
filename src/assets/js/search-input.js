c665f8b3c9bb8a8d4e914c0ed8bfda2855706dca
Evan Williams <evwilliams@linkedin.com>
2020-05-04 20:24:18 -0700

    Fix page load delays caused by search script (#280)
    For some reason, the search script was enabled on all pages. This was an
    issue because the search script loads up all textual content on the site
    into memory. This was causing long page load times on the live website.
    This addresses that by only enabling the search functionality on the
    search page. It also cleans up the search scripts.

---
+++ src/assets/js/search-input.js
@@ -0,0 +1,17 @@
+---
+---
+
+/*
+ * This script just allows the search bar to navigate the user to the search page.
+ */
+$(document).ready(() => {
+  const searchInput = $('#search-input');
+  searchInput.keydown((event) => {
+    const enterPressed = event.which === 13;
+    const query = searchInput.val().trim();
+    if (enterPressed && query) {
+      event.preventDefault();
+      window.location.href = `{{ 'search' | relative_url }}?term=${query}`;
+    }
+  });
+});
