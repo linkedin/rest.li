d6cbc094e725cdc06850eb9ef5c9b13549d5245c
Nizar Mankulangara <nmankula@nmankula-mn1.linkedin.biz>
2020-05-14 11:06:18 -0700

    http/2 parent channel creation timeout

--- r2-netty/src/main/java/com/linkedin/r2/netty/client/http2/Http2ChannelLifecycle.java
+++ r2-netty/src/main/java/com/linkedin/r2/netty/client/http2/Http2ChannelLifecycle.java
@@ -19,7 +19,9 @@ package com.linkedin.r2.netty.client.http2;
 import com.linkedin.common.callback.Callback;
 import com.linkedin.r2.netty.common.NettyChannelAttributes;
 import com.linkedin.r2.transport.http.client.AsyncPool;
+import com.linkedin.r2.transport.http.client.ObjectCreationTimeoutException;
 import com.linkedin.r2.transport.http.client.PoolStats;
+import com.linkedin.r2.transport.http.client.TimeoutCallback;
 import com.linkedin.util.clock.Clock;
 import io.netty.channel.Channel;
 import io.netty.channel.group.ChannelGroup;
@@ -51,6 +53,7 @@ import org.slf4j.LoggerFactory;
 class Http2ChannelLifecycle implements AsyncPool.Lifecycle<Channel>
 {
   private static final Logger LOG = LoggerFactory.getLogger(Http2ChannelLifecycle.class);
+  public static final int DEFAULT_OBJECT_CREATION_TIMEOUT = 10000;
 
   private final SocketAddress _address;
   private final ScheduledExecutorService _scheduler;
@@ -59,6 +62,7 @@ class Http2ChannelLifecycle implements AsyncPool.Lifecycle<Channel>
   private final long _maxContentLength;
   private final long _idleTimeout;
   private AsyncPool.Lifecycle<Channel> _parentChannelLifecycle;
+  private final long _creationTimeout;
 
   /**
    * Read and write to the following members should be synchronized by this lock.
@@ -70,6 +74,7 @@ class Http2ChannelLifecycle implements AsyncPool.Lifecycle<Channel>
   private Channel _parentChannel = null;
   private long _childChannelCount;
   private long _lastActiveTime;
+
 
   Http2ChannelLifecycle(SocketAddress address, ScheduledExecutorService scheduler, Clock clock,
       ChannelGroup channelGroup, boolean ssl, long maxContentLength, long idleTimeout, AsyncPool.Lifecycle<Channel> parentChannelLifecycle)
@@ -83,8 +88,8 @@ class Http2ChannelLifecycle implements AsyncPool.Lifecycle<Channel>
     _idleTimeout = idleTimeout;
     _parentChannelLifecycle = parentChannelLifecycle;
     _childChannelCount = 0;
-
     _lastActiveTime = _clock.currentTimeMillis();
+    _creationTimeout = DEFAULT_OBJECT_CREATION_TIMEOUT; // TODO: expose this through cfg2
     _scheduler.scheduleAtFixedRate(this::closeParentIfIdle, idleTimeout, idleTimeout, TimeUnit.MILLISECONDS);
   }
 
@@ -186,7 +191,10 @@ class Http2ChannelLifecycle implements AsyncPool.Lifecycle<Channel>
    */
   private void doBootstrapParentChannel(Callback<Channel> callback)
   {
-    _parentChannelLifecycle.create(new Callback<Channel>() {
+    // Lets not trust the _parentChannelLifecycle to timely return a response here.
+    // Embedding the callback inside a timeout callback (ObjectCreationTimeoutCallback)
+    // to force a response within creationTimeout deadline
+    _parentChannelLifecycle.create(new TimeoutCallback<>(_scheduler, _creationTimeout, TimeUnit.MILLISECONDS, new Callback<Channel>() {
       @Override
       public void onError(Throwable error)
       {
@@ -207,7 +215,8 @@ class Http2ChannelLifecycle implements AsyncPool.Lifecycle<Channel>
           }
         });
       }
-    });
+    }, new ObjectCreationTimeoutException(
+      "HTTP/2 parent channel creation Exceeded creation timeout of " + _creationTimeout)));
   }
 
   /**
