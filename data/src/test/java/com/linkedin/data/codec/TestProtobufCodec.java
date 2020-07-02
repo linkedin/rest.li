f9f1c898e897a18a15bcf04c3abf9c553e5e938a
karthikrg <karthikrg.bits@gmail.com>
2020-07-01 22:50:45 -0700

    Add an option (enabled by default) to gracefully degrade on encountering invalid surrogate pairs during protobuf string serialization (#344)

--- data/src/test/java/com/linkedin/data/codec/TestProtobufCodec.java
+++ data/src/test/java/com/linkedin/data/codec/TestProtobufCodec.java
@@ -17,7 +17,10 @@
 package com.linkedin.data.codec;
 
 import com.linkedin.data.DataComplex;
+import com.linkedin.data.DataMap;
+import com.linkedin.data.protobuf.Utf8Utils;
 import java.io.IOException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestProtobufCodec extends TestCodec
@@ -35,5 +38,43 @@ public class TestProtobufCodec extends TestCodec
           .setEnableFixedLengthFloatDoubles(enableFixedLengthFloatDoubles)
           .build());
     testDataCodec(codec, dataComplex);
+  }
+
+  @Test(dataProvider = "surrogatePairData", dataProviderClass = CodecDataProviders.class)
+  public void testSurrogatePairs(String value, String expectedString, int expectedLength,
+      boolean isValidSurrogatePair, boolean tolerateInvalidSurrogatePairs) throws Exception
+  {
+    DataCodec codec =
+        new ProtobufDataCodec(new ProtobufCodecOptions.Builder()
+            .setShouldTolerateInvalidSurrogatePairs(tolerateInvalidSurrogatePairs)
+            .build());
+
+    if (isValidSurrogatePair) {
+      Assert.assertEquals(Utf8Utils.encodedLength(value, true), expectedLength);
+      Assert.assertEquals(Utf8Utils.encodedLength(value, false), expectedLength);
+    } else {
+      Assert.assertEquals(Utf8Utils.encodedLength(value, true), expectedLength);
+      try {
+        Utf8Utils.encodedLength(value, false);
+        Assert.fail("Exception not thrown for invalid surrogate pair");
+      } catch (IllegalArgumentException e) {
+        // Success.
+      }
+    }
+
+    DataMap dataMap = new DataMap();
+    dataMap.put("key", value);
+
+    if (isValidSurrogatePair || tolerateInvalidSurrogatePairs) {
+      DataMap roundtrip = codec.bytesToMap(codec.mapToBytes(dataMap));
+      Assert.assertEquals(roundtrip.get("key"), expectedString);
+    } else {
+      try {
+        codec.bytesToMap(codec.mapToBytes(dataMap));
+        Assert.fail("Exception not thrown for invalid surrogate pair");
+      } catch (IOException e) {
+        // Success.
+      }
+    }
   }
 }
