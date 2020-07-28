be1c35adcf7a5179a0e04adb723d6593aafb42a9
rhan <rhan@linkedin.com>
2020-07-28 10:53:53 -0700

    Add copyright info

--- d2-int-test/src/test/java/com/linkedin/d2/loadbalancer/strategies/TestLoadBalancerPerformanceSimulation.java
+++ d2-int-test/src/test/java/com/linkedin/d2/loadbalancer/strategies/TestLoadBalancerPerformanceSimulation.java
@@ -1,3 +1,19 @@
+/*
+   Copyright (c) 2020 LinkedIn Corp.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package com.linkedin.d2.loadbalancer.strategies;
 
 import com.linkedin.d2.D2RelativeStrategyProperties;
@@ -15,7 +31,8 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 
 /**
@@ -99,7 +116,7 @@ public class TestLoadBalancerPerformanceSimulation {
     testRunner.runWait();
     Map<URI, Integer> pointsMap = testRunner.getPoints();
 
-    assertEquals(pointsMap.get(testRunner.getUri(0)).intValue(),  1);
+    assertEquals(pointsMap.get(testRunner.getUri(0)).intValue(),  UNHEALTHY_POINTS);
   }
 
   @Test(dataProvider = "relativeLatencyLowThresholdFactor")
