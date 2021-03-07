86f8ca2fb3752167013ac01e3e9c4044e38fc7b6
Rick Zhou <riczhou@linkedin.com>
2021-03-07 00:02:21 -0800

    add simple resource

---
+++ restli-tools/src/main/java/com/linkedin/restli/tools/clientgen/fluentspec/SimpleResourceSpec.java
@@ -0,0 +1,37 @@
+/*
+   Copyright (c) 2021 LinkedIn Corp.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package com.linkedin.restli.tools.clientgen.fluentspec;
+
+import com.linkedin.data.schema.DataSchemaResolver;
+import com.linkedin.pegasus.generator.TemplateSpecGenerator;
+import com.linkedin.pegasus.generator.spec.ClassTemplateSpec;
+import com.linkedin.restli.restspec.ResourceSchema;
+
+
+public class SimpleResourceSpec extends BaseResourceSpec
+{
+
+  private ClassTemplateSpec _entityClass;
+
+  // Simple resource only supports get, update/partial_update, delete and actions
+  public SimpleResourceSpec(ResourceSchema resourceSchema, TemplateSpecGenerator templateSpecGenerator,
+      String sourceIdlName, DataSchemaResolver schemaResolver)
+  {
+    super(resourceSchema, templateSpecGenerator, sourceIdlName, schemaResolver);
+  }
+
+}
