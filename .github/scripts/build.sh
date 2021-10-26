7a26da2f1f195caf83a98b128c63eceded118fa0
Evan Williams <evwilliams@linkedin.com>
2021-10-26 13:20:41 -0700

    Fetch base ref so it's present in the workflow

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -33,6 +33,8 @@ fi
 
 # For PR builds only...
 if [ ! -z "$GITHUB_HEAD_REF" ] && [ ! -z "$GITHUB_BASE_REF" ]; then
+  # Fetch the PR base ref so it can be used to generate diffs
+  git fetch origin $GITHUB_BASE_REF
   # If the project version is being bumped in this PR, assert that the changelog contains an entry for it
   if (! $RELEASE_CANDIDATE) &&
       (git diff ${GITHUB_BASE_REF}...HEAD -- gradle.properties | grep -F "+version=$VERSION" > /dev/null) &&
