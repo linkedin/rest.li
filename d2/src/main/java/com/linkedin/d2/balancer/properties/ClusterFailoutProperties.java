9c54b48bac4edc98aae11fdd1a64d1cde03c4920
Chris Stufflebeam <cstufflebeam@linkedin.com>
2022-03-02 16:34:47 -0800

    Added support for cluster level failouts (SLF)

---
+++ d2/src/main/java/com/linkedin/d2/balancer/properties/ClusterFailoutProperties.java
@@ -0,0 +1,83 @@
+package com.linkedin.d2.balancer.properties;
+
+import java.util.Map;
+import java.util.List;
+/*
+   Copyright (c) 2012 LinkedIn Corp.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+import java.util.stream.Collectors;
+import java.util.Collections;
+
+/**
+ * Configuration for a service's cluster level failout properties. These properties are used to control
+ * the flow of traffic between datacenters.
+ */
+public class ClusterFailoutProperties
+{
+  private final List<Map<String, Object>> _clusterFailoutRedirectConfigs;
+  private final List<Map<String, Object>> _clusterFailoutBucketConfigs;
+
+  public ClusterFailoutProperties(List<Map<String, Object>> clusterFailoutRedirectConfigs,
+      List<Map<String, Object>> clusterFailoutBucketConfigs)
+  {
+    _clusterFailoutBucketConfigs = clusterFailoutBucketConfigs;
+    _clusterFailoutRedirectConfigs = clusterFailoutRedirectConfigs;
+  }
+
+  public List<Map<String, Object>> getClusterFailoutRedirectConfigs()
+  {
+    return _clusterFailoutRedirectConfigs;
+  }
+
+  public List<Map<String, Object>> getClusterFailoutBucketConfigs()
+  {
+    return _clusterFailoutBucketConfigs;
+  }
+
+  @Override
+  public String toString()
+  {
+    return "ClusterFailoutProperties [_clusterFailoutRedirectConfigs=" + _clusterFailoutRedirectConfigs
+        + ", _clusterFailoutBucketConfigs=" + _clusterFailoutBucketConfigs
+        + "]";
+  }
+
+  @Override
+  public int hashCode()
+  {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + _clusterFailoutRedirectConfigs.hashCode();
+    result = prime * result + _clusterFailoutBucketConfigs.hashCode();
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+
+    ClusterFailoutProperties other = (ClusterFailoutProperties) obj;
+    return _clusterFailoutRedirectConfigs.equals(other.getClusterFailoutRedirectConfigs()) &&
+          _clusterFailoutBucketConfigs.equals(other.getClusterFailoutBucketConfigs());
+  }
+}
