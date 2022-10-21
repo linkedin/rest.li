11a0a38beec34cfa16efcbd402300af34f484918
Mahesh Vishwanath <mvishwan@mvishwan-mn1.linkedin.biz>
2022-10-21 00:35:11 -0700

    [rest.li] Update IndentedPdlBuilder to remove extra whitespace when writing a blank comment line

--- data/src/main/java/com/linkedin/data/schema/IndentedPdlBuilder.java
+++ data/src/main/java/com/linkedin/data/schema/IndentedPdlBuilder.java
@@ -115,7 +115,10 @@ class IndentedPdlBuilder extends PdlBuilder
       writeLine("/**");
       for (String line : doc.split("\n"))
       {
-        indent().write(" * ").write(line).newline();
+        String commentPrefix = line.isBlank()
+          ? " *"
+          : " * ";
+        indent().write(commentPrefix).write(line).newline();
       }
       writeLine(" */");
       return true;
