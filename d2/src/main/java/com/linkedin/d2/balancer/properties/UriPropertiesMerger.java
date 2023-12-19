904c82572d3323742f3ba0067e3c1e57a11b8f11
Bohan Yang <boyang@linkedin.com>
2023-12-19 15:49:52 -0800

    modify the log msg

--- d2/src/main/java/com/linkedin/d2/balancer/properties/UriPropertiesMerger.java
+++ d2/src/main/java/com/linkedin/d2/balancer/properties/UriPropertiesMerger.java
@@ -55,7 +55,7 @@ public class UriPropertiesMerger implements ZooKeeperPropertyMerger<UriPropertie
 
     if (maxVersion == -1)
     {
-      LOG.warn("Merged Uri properties has invalid version -1. It should be > -1.");
+      LOG.warn("Merged Uri properties for cluster {} has invalid version -1. It should be > -1.", propertyName);
     }
     return new UriProperties(clusterName, partitionData, uriSpecificProperties, maxVersion);
   }
