0dbe0b3091d65b854c54214a0b175dd50f845267
brycezhongqing <zhonchen+1@linkedin.com>
2024-03-22 09:34:53 -0700

    Catch up more potential exception for dual-read

--- d2/src/main/java/com/linkedin/d2/balancer/dualread/DualReadLoadBalancer.java
+++ d2/src/main/java/com/linkedin/d2/balancer/dualread/DualReadLoadBalancer.java
@@ -116,6 +116,10 @@ public class DualReadLoadBalancer implements LoadBalancerWithFacilities
       _rateLimitedLogger.debug("newLb executor rejected new task for start. "
           + "It is shut down or its queue size has reached max limit");
     }
+    catch (Exception e)
+    {
+      _rateLimitedLogger.debug("Failed to execute newLb task for start. ", e);
+    }
 
     _oldLb.start(getStartUpCallback(false,
         mode == DualReadModeProvider.DualReadMode.NEW_LB_ONLY ? null : callback
@@ -206,6 +210,10 @@ public class DualReadLoadBalancer implements LoadBalancerWithFacilities
           _rateLimitedLogger.debug("newLb executor rejected new task for getClient. "
               + "It is shut down or its queue size has reached max limit");
         }
+        catch (Exception e)
+        {
+          _rateLimitedLogger.debug("Failed to execute newLb task for getClient. ", e);
+        }
 
         _oldLb.getClient(request, requestContext, clientCallback);
         break;
@@ -233,6 +241,10 @@ public class DualReadLoadBalancer implements LoadBalancerWithFacilities
           _rateLimitedLogger.debug("newLb executor rejected new task for getLoadBalancedServiceProperties. "
               + "It is shut down or its queue size has reached max limit");
         }
+        catch (Exception e)
+        {
+          _rateLimitedLogger.debug("Failed to execute newLb task for getLoadBalancedServiceProperties. ", e);
+        }
         _oldLb.getLoadBalancedServiceProperties(serviceName, clientCallback);
         break;
       case OLD_LB_ONLY:
@@ -259,6 +271,10 @@ public class DualReadLoadBalancer implements LoadBalancerWithFacilities
         {
           _rateLimitedLogger.debug("newLb executor rejected new task for getLoadBalancedClusterAndUriProperties. "
               + "It is shut down or its queue size has reached max limit");
+        }
+        catch (Exception e)
+        {
+          _rateLimitedLogger.debug("Failed to execute newLb task for getLoadBalancedClusterAndUriProperties. ", e);
         }
         _oldLb.getLoadBalancedClusterAndUriProperties(clusterName, callback);
         break;
