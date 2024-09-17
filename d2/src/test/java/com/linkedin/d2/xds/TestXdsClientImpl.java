dad7e09db78d9ddffa50280b2b126e286d7ebf4a
brycezhongqing <zhonchen+1@linkedin.com>
2024-09-16 20:34:08 -0700

    resolve all the comments

--- d2/src/test/java/com/linkedin/d2/xds/TestXdsClientImpl.java
+++ d2/src/test/java/com/linkedin/d2/xds/TestXdsClientImpl.java
@@ -16,7 +16,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import io.grpc.Status;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
