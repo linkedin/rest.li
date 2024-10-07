873f9a62cb6a9e960d7ea4ef4d52ed0158f1ccf8
brycezhongqing <zhonchen+1@linkedin.com>
2024-10-07 16:19:50 -0700

    delete deprecate.sh

--- deprecate.sh
+++
@@ -1,8 +0,0 @@
-grep -o "include '[^']*'" settings.gradle | sed -e "s/^include '/com.linkedin.pegasus:/g" -e "s/'//g" | while read -r module_name ; do
-    if [ "$module_name" == "com.linkedin.pegasus:gradle-plugins" ]
-    then
-        echo "WARNING: $module_name cannot be deprecated due to MPPCX-7165. Skipping deprecation..."
-    else
-        mint catalog deprecate "$module_name" "$@"
-    fi
-done
